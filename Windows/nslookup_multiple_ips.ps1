2780431a271609ce0cfeefc0bd9d360193071218
0xph03n1x <lazarovmihail97@gmail.com>
2022-01-27 16:22:53 +0200

    commit4

---
+++ Windows/nslookup_multiple_ips.ps1
@@ -0,0 +1,22 @@
+$InputFile = '<path to IP list>'
+$addresses = get-content $InputFile
+$reader = New-Object IO.StreamReader $InputFile
+    while($reader.ReadLine() -ne $null){ $TotalIPs++ }
+write-host  ""
+write-host "Performing nslookup on each address..."
+        for each($address in addresses) {
+            ## Progress bar
+            $i++
+            $percentagedone = (($i / $TotalIPs) * 100)
+            $percentagerounded = "{0:N0}" -f $percentagedone
+            Write-Progress -Activity "Performing nslookups" -CurrentOperation "Working on IP:  $address (IP $i of $TotalIPs)" -Status "$percentagerounded% complete" -PercentageComplete $percentagedone
+            ## End Progress bar
+            try {
+                [system.net.dns]::resolve($address) | Select AddressList,Hostname | Format-Table -AutoSize | Out-File lookup.csv
+            }
+            catch {
+                Write-host "$address was not found. $_" -ForegroundColor Green
+            }
+        }
+write-host  ""
+write-host "Done!"
