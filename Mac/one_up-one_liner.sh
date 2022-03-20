e1e46ec74a5e69bbab34c3f88dc3cddcdd9c2898
0xph03n1x <lazarovmihail97@gmail.com>
2022-03-21 01:27:23 +0200

    Update the one up one-liner

--- Mac/one_up-one_liner.sh
+++ Mac/one_up-one_liner.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
+
 find . -name "*.mkv" -exec mv {} --target ~/Downloads/Private\ Eyes \;
+
+#Might only work for less than 10 items
+for i in {1..9}; do cd Penny.Dreadful.S03E0$i.576p.WEBRIP.x264.AC3-GOD && mv *.mkv .. && cd ..; done
+
+#Remove the remaining folders
+for i in {1..9}; do rm -rf Penny.Dreadful.S03E0$i.576p.WEBRIP.x264.AC3-GOD; done
