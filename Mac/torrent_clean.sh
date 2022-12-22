5cba2cdddc5bfc50d66ffbbe78c0e09b3303021c
0xph03n1x <lazarovmihail97@gmail.com>
2022-12-22 20:18:08 +0100

    torr

--- Mac/torrent_clean.sh
+++ Mac/torrent_clean.sh
@@ -4,5 +4,5 @@ rm -v ~/Downloads/*.torrent
 for f in ~/Downloads/*.torrent
 do
 	# if found, delete it
-    [ -f "$f" ] echo "removed $f && rm "$f"
+    [ -f "$f" ] echo "removed $f" && rm "$f"
 done
