f0dd91be6674a69e6fdb02e629e0c45b338cf6e0
0xph03n1x <lazarovmihail97@gmail.com>
2024-12-28 21:27:44 +0200

    update target

--- Mac/one_up-one_liner.sh
+++ Mac/one_up-one_liner.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+# move files to current dir
 find . -name "*.mkv" -exec mv {} --target . \;
+
+# remove directories from current dir
+find . -type d -exec rm -rf {} \;
 
 # Simple example of loop to move files from subdirectories to the current directory
 for i in {01..10}; do mv folder$i/file$i .; done
